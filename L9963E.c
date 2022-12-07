b055e5e9ce4b03d7ac871e584f5fc30d8a317325
Federico Carbone <federico.carbone20@gmail.com>
2022-12-07 16:27:26 +0100

    chore: fixed endiannes and removed useless declarations

--- L9963E.c
+++ L9963E.c
@@ -9,124 +9,91 @@
  */
 
 #include "L9963E.h"
+
 #include <string.h>
 
-/*
-#include <stdio.h>
-#include <inttypes.h>
+const uint16_t CrcCalc8bitLookupTab_B[] = {
+    0x3123, 0x0005, 0x8474, 0x3143, 0x0007, 0x1809, 0xa800, 0x74e0, 0x0031, 0x9404, 0xe228, 0x3104, 0x0000, 0x18aa,
+    0xa801, 0x8603, 0x7d07, 0x0278, 0x74e5, 0x063f, 0x7d63, 0x2a14, 0x316b, 0x000b, 0x7a05, 0x00fe, 0x192a, 0x84fe,
+    0x7c8c, 0x2378, 0x7528, 0x063f, 0x1808, 0x8001, 0x00b0, 0x18ec, 0x0001, 0x7ce6, 0x5a78, 0x7ca3, 0x3214, 0x3165,
+    0x000b, 0x7a20, 0xfff0, 0x7c84, 0x50ae, 0x7d67, 0x2278, 0xe844, 0x1989, 0xb008, 0x8054, 0x3104, 0x0001, 0x18aa,
+    0xa801, 0x758c, 0x063f, 0x8663, 0x7cab, 0x4830, 0x7d07, 0x6630, 0x7d60, 0x3b78, 0x7c8b, 0x2378, 0x7c05, 0x3278,
+    0x4816, 0x74a7, 0x063f, 0x7ca3, 0x3a14, 0x8b55, 0x7a05, 0x0090, 0x1906, 0x8001, 0x18eb, 0x0001, 0x7506, 0x063f,
+    0x7ce7, 0x4830, 0x7d04, 0x30ae, 0x7c06, 0x5040, 0x7d00, 0x6630, 0x4407, 0x74e0, 0x063f, 0x7ca5, 0x0278, 0x7ce3,
+    0x2a14, 0x8b57, 0xe2ea, 0x1946, 0x8001, 0x754b, 0x063f, 0x7c84, 0x58ae, 0x7d08, 0x4830, 0x7c89, 0x6630, 0x7d2c,
+    0x4378, 0x7ca6, 0x6278, 0x74c7, 0x063f, 0x3143, 0x0008, 0x8a03, 0x180a, 0xa800, 0xe614, 0x192a, 0x84ff, 0x6386,
+    0x752c, 0x063f, 0x190c, 0x8001, 0x7d09, 0x03a6, 0x7c0b, 0x3a78, 0x7cc8, 0x3839, 0x6810, 0x6816, 0x7ce7, 0x589e,
+    0x7a20, 0xfff0, 0x8933, 0x7ce5, 0x1e30, 0x74a3, 0x063f, 0x0004, 0x7160, 0x0002, 0xe8d3, 0x7140, 0x0001, 0xe894};
 
-void init_CRC6_Table()
-{
-        uint64_t remainder;
-        for(uint8_t dividend=0; dividend < 64; ++dividend) {
-                uint8_t currByte = dividend;
-                for(uint8_t i=0; i<6; ++i){
-                        if(currByte & 0x20){
-                                currByte <<= 1;
-                        } else {
-                                currByte <<= 1;
-                                currByte ^= 0b0011001;
-                        }
-                }
-                printf("0x%x", currByte & 0x3f);
-                if(dividend != 63)
-                    printf(", ");
-        }
+#define WORD_LEN           40UL
+#define CRC_LEN            6UL
+#define CRC_POLY           (uint64_t)0x0000000000000059 /*L9301 CRC Poly:x^3+x^2+x+1*/
+#define CRC_SEED           (uint64_t)0x0000000000000038
+#define CRC_INIT_SEED_MASK (CRC_SEED << (WORD_LEN - CRC_LEN))
+#define CRC_INIT_MASK      (CRC_POLY << (WORD_LEN - CRC_LEN - 1))
+#define FIRST_BIT_MASK     ((uint64_t)1 << (WORD_LEN - 1))  // 0x80000000
+#define CRC_LOWER_MASK     ((uint8_t)(1 << CRC_LEN) - 1)    //0b111
+
+uint8_t L9963E_crc_calc(uint64_t InputWord) {
+    uint64_t TestBitMask;
+    uint64_t CRCMask;
+    uint64_t BitCount;
+    uint64_t LeftAlignedWord;
+
+    InputWord &= 0xFFFFFFFFFFFFFFC0; /* Clear the CRC bit in the data frame*/
+    LeftAlignedWord = InputWord ^ CRC_INIT_SEED_MASK;
+
+    TestBitMask = ((uint64_t)1 << (WORD_LEN - 1));
+    CRCMask     = CRC_INIT_MASK;  // 1111 <<
+    BitCount    = (WORD_LEN - CRC_LEN);
+    while (0 != BitCount--) {
+        if (0 != (LeftAlignedWord & TestBitMask)) {
+            LeftAlignedWord ^= CRCMask;
+        } /* endif */
+        CRCMask >>= 1;
+        TestBitMask >>= 1;
+    } /* endwhile */
+
+    LeftAlignedWord &= (uint64_t)CRC_LOWER_MASK;
+    return LeftAlignedWord;
 }
 
-void main(void) {
-        printf("{");
-        init_CRC6_Table();
-        printf("}");
-        return;
-}
-*/
-const uint16_t CrcCalc8bitLookupTab_B[] =
-{ 0x3123, 0x0005, 0x8474, 0x3143, 0x0007, 0x1809, 0xa800, 0x74e0, 0x0031,
-		0x9404, 0xe228, 0x3104, 0x0000, 0x18aa, 0xa801, 0x8603, 0x7d07, 0x0278,
-		0x74e5, 0x063f, 0x7d63, 0x2a14, 0x316b, 0x000b, 0x7a05, 0x00fe, 0x192a,
-		0x84fe, 0x7c8c, 0x2378, 0x7528, 0x063f, 0x1808, 0x8001, 0x00b0, 0x18ec,
-		0x0001, 0x7ce6, 0x5a78, 0x7ca3, 0x3214, 0x3165, 0x000b, 0x7a20, 0xfff0,
-		0x7c84, 0x50ae, 0x7d67, 0x2278, 0xe844, 0x1989, 0xb008, 0x8054, 0x3104,
-		0x0001, 0x18aa, 0xa801, 0x758c, 0x063f, 0x8663, 0x7cab, 0x4830, 0x7d07,
-		0x6630, 0x7d60, 0x3b78, 0x7c8b, 0x2378, 0x7c05, 0x3278, 0x4816, 0x74a7,
-		0x063f, 0x7ca3, 0x3a14, 0x8b55, 0x7a05, 0x0090, 0x1906, 0x8001, 0x18eb,
-		0x0001, 0x7506, 0x063f, 0x7ce7, 0x4830, 0x7d04, 0x30ae, 0x7c06, 0x5040,
-		0x7d00, 0x6630, 0x4407, 0x74e0, 0x063f, 0x7ca5, 0x0278, 0x7ce3, 0x2a14,
-		0x8b57, 0xe2ea, 0x1946, 0x8001, 0x754b, 0x063f, 0x7c84, 0x58ae, 0x7d08,
-		0x4830, 0x7c89, 0x6630, 0x7d2c, 0x4378, 0x7ca6, 0x6278, 0x74c7, 0x063f,
-		0x3143, 0x0008, 0x8a03, 0x180a, 0xa800, 0xe614, 0x192a, 0x84ff, 0x6386,
-		0x752c, 0x063f, 0x190c, 0x8001, 0x7d09, 0x03a6, 0x7c0b, 0x3a78, 0x7cc8,
-		0x3839, 0x6810, 0x6816, 0x7ce7, 0x589e, 0x7a20, 0xfff0, 0x8933, 0x7ce5,
-		0x1e30, 0x74a3, 0x063f, 0x0004, 0x7160, 0x0002, 0xe8d3, 0x7140, 0x0001,
-		0xe894 };
-
-
-#define WORD_LEN 40UL
-#define CRC_LEN 6UL
-#define CRC_POLY (uint64_t)0x0000000000000059 /*L9301 CRC Poly:x^3+x^2+x+1*/
-#define CRC_SEED (uint64_t)0x0000000000000038
-#define CRC_INIT_SEED_MASK (CRC_SEED<<(WORD_LEN-CRC_LEN))
-#define CRC_INIT_MASK (CRC_POLY<<(WORD_LEN-CRC_LEN-1))
-#define FIRST_BIT_MASK ((uint64_t)1<<(WORD_LEN-1))  // 0x80000000
-#define CRC_LOWER_MASK ((uint8_t)(1<<CRC_LEN)-1) //0b111
-
-uint8_t CrcCompute_L9963(uint64_t InputWord)
-{
-	uint64_t TestBitMask;
-	uint64_t CRCMask;
-	uint64_t BitCount;
-	uint64_t LeftAlignedWord;
-
-	InputWord &= 0xFFFFFFFFFFFFFFC0; /* Clear the CRC bit in the data frame*/
-	LeftAlignedWord = InputWord ^ CRC_INIT_SEED_MASK;
-
-	TestBitMask = ((uint64_t) 1 << ( WORD_LEN - 1));
-	CRCMask = CRC_INIT_MASK; // 1111 <<
-	BitCount = ( WORD_LEN - CRC_LEN);
-	while (0 != BitCount--)
-	{
-		if (0 != (LeftAlignedWord & TestBitMask))
-		{
-			LeftAlignedWord ^= CRCMask;
-		} /* endif */
-		CRCMask >>= 1;
-		TestBitMask >>= 1;
-	} /* endwhile */
-
-	LeftAlignedWord &= (uint64_t)CRC_LOWER_MASK;
-	return LeftAlignedWord;
-}
-
-
-HAL_StatusTypeDef L9963E_init(L9963E_HandleTypeDef *handle, SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port, uint16_t cs_pin, GPIO_TypeDef *txen_port, uint16_t txen_pin, GPIO_TypeDef *bne_port, uint16_t bne_pin) {
-    if(handle == NULL) {
+HAL_StatusTypeDef L9963E_init(
+    L9963E_HandleTypeDef *handle,
+    SPI_HandleTypeDef *hspi,
+    GPIO_TypeDef *cs_port,
+    uint16_t cs_pin,
+    GPIO_TypeDef *txen_port,
+    uint16_t txen_pin,
+    GPIO_TypeDef *bne_port,
+    uint16_t bne_pin) {
+    if (handle == NULL) {
         return HAL_ERROR;
     }
 
-    if(hspi == NULL) {
+    if (hspi == NULL) {
         return HAL_ERROR;
     }
 
-    if(cs_port == NULL) {
+    if (cs_port == NULL) {
         return HAL_ERROR;
     }
 
-    if(txen_port == NULL) {
+    if (txen_port == NULL) {
         return HAL_ERROR;
     }
 
-    if(bne_port == NULL) {
+    if (bne_port == NULL) {
         return HAL_ERROR;
     }
 
-    handle->hspi = hspi;
-    handle->cs_port = cs_port;
-    handle->cs_pin = cs_pin;
+    handle->hspi      = hspi;
+    handle->cs_port   = cs_port;
+    handle->cs_pin    = cs_pin;
     handle->txen_port = txen_port;
-    handle->txen_pin = txen_pin;
-    handle->bne_port = bne_port;
-    handle->bne_pin = bne_pin;
+    handle->txen_pin  = txen_pin;
+    handle->bne_port  = bne_port;
+    handle->bne_pin   = bne_pin;
 
     L9963E_CS_HIGH(handle);
     L9963E_TXEN_HIGH(handle);
@@ -138,7 +105,7 @@ HAL_StatusTypeDef L9963E_wakeup(L9963E_HandleTypeDef *handle) {
     uint8_t dummy[5];
     HAL_StatusTypeDef errorcode = HAL_OK;
 
-    if(handle == NULL) {
+    if (handle == NULL) {
         return HAL_ERROR;
     }
     L9963E_CS_LOW(handle);
@@ -148,153 +115,94 @@ HAL_StatusTypeDef L9963E_wakeup(L9963E_HandleTypeDef *handle) {
     return errorcode;
 }
 
-HAL_StatusTypeDef _L9963E_reg_cmd(L9963E_HandleTypeDef *handle, uint8_t is_write, uint8_t device, L9963E_RegistersAddrTypeDef address, L9963E_RegisterUnionTypeDef *data) {
-    union {
-        L9963E_CmdTypeDef cmd;
-        uint64_t val;
-    } dat;
+HAL_StatusTypeDef _L9963E_reg_cmd(
+    L9963E_HandleTypeDef *handle,
+    uint8_t is_write,
+    uint8_t device,
+    L9963E_RegistersAddrTypeDef address,
+    L9963E_RegisterUnionTypeDef *data) {
+    union L9963E_CmdUnion dat;
     HAL_StatusTypeDef errorcode = HAL_OK;
     uint32_t current_tick;
-
-    if(handle == NULL) {
-        return HAL_ERROR;
-    }
-
-    if(data == NULL) {
-        return HAL_ERROR;
-    }
-
-    dat.cmd.pa = 1;
-    dat.cmd.rw_burst = is_write ? 1 : 0;
-    dat.cmd.devid = device;
-    dat.cmd.addr = address,
-    dat.cmd.data = data->generic;
-    dat.cmd.crc = CrcCompute_L9963(dat.val);
-
     uint8_t d[5];
-    memcpy(d, (uint8_t*)&dat.val, 5);
+
+    if (handle == NULL) {
+        return HAL_ERROR;
+    }
+
+    if (data == NULL) {
+        return HAL_ERROR;
+    }
+
+    dat.cmd.pa       = 1;
+    dat.cmd.rw_burst = is_write ? 1 : 0;
+    dat.cmd.devid    = device;
+    dat.cmd.addr = address, dat.cmd.data = data->generic;
+    dat.cmd.crc = L9963E_crc_calc(dat.val);
+
+    d[0] = *((uint8_t *)&dat.val + 4);
+    d[1] = *((uint8_t *)&dat.val + 3);
+    d[2] = *((uint8_t *)&dat.val + 2);
+    d[3] = *((uint8_t *)&dat.val + 1);
+    d[4] = *((uint8_t *)&dat.val + 0);
 
     L9963E_CS_LOW(handle);
-    errorcode = HAL_SPI_Transmit(handle->hspi, (uint8_t*)&dat.val, 5, 10);
+    errorcode = HAL_SPI_Transmit(handle->hspi, d, 5, 10);
     L9963E_CS_HIGH(handle);
 
-    if(errorcode != HAL_OK) {
+    if (errorcode != HAL_OK) {
         return errorcode;
     }
 
-    dat.cmd.addr = -1;
+    dat.cmd.addr  = -1;
     dat.cmd.devid = -1;
 
     current_tick = HAL_GetTick();
-    while(dat.cmd.addr != address && dat.cmd.devid != device) {
-        while(L9963E_BNE_READ(handle) == GPIO_PIN_RESET) {
-            if(HAL_GetTick() - current_tick > 10) {
+    while (dat.cmd.addr != address && dat.cmd.devid != device) {
+        while (L9963E_BNE_READ(handle) == GPIO_PIN_RESET) {
+            if (HAL_GetTick() - current_tick > 10) {
                 return HAL_TIMEOUT;
             }
         }
-        
+
         L9963E_TXEN_LOW(handle);
         L9963E_CS_LOW(handle);
-        errorcode = HAL_SPI_Receive(handle->hspi, (uint8_t*)&dat.val, 5, 100);
+        errorcode = HAL_SPI_Receive(handle->hspi, d, 5, 100);
         L9963E_CS_HIGH(handle);
         L9963E_TXEN_HIGH(handle);
 
-        if(errorcode != HAL_OK) {
+        *((uint8_t *)&dat.val + 4) = d[0];
+        *((uint8_t *)&dat.val + 3) = d[1];
+        *((uint8_t *)&dat.val + 2) = d[2];
+        *((uint8_t *)&dat.val + 1) = d[3];
+        *((uint8_t *)&dat.val + 0) = d[4];
+
+        if (errorcode != HAL_OK) {
             return errorcode;
         }
     }
-    
+
     data->generic = dat.cmd.data;
 
     return HAL_OK;
 }
 
-HAL_StatusTypeDef L9963E_reg_read(L9963E_HandleTypeDef *handle, uint8_t device, L9963E_RegistersAddrTypeDef address, L9963E_RegisterUnionTypeDef *data) {
-    if(device == 0) {
+HAL_StatusTypeDef L9963E_reg_read(
+    L9963E_HandleTypeDef *handle,
+    uint8_t device,
+    L9963E_RegistersAddrTypeDef address,
+    L9963E_RegisterUnionTypeDef *data) {
+    if (device == 0) {
         return HAL_ERROR;
     }
 
     return _L9963E_reg_cmd(handle, 0, device, address, data);
 }
 
-HAL_StatusTypeDef L9963E_reg_write(L9963E_HandleTypeDef *handle, uint8_t device, L9963E_RegistersAddrTypeDef address, L9963E_RegisterUnionTypeDef *data) {
+HAL_StatusTypeDef L9963E_reg_write(
+    L9963E_HandleTypeDef *handle,
+    uint8_t device,
+    L9963E_RegistersAddrTypeDef address,
+    L9963E_RegisterUnionTypeDef *data) {
     return _L9963E_reg_cmd(handle, 1, device, address, data);
-}
-
-uint8_t L9963E_crc_calc(L9963E_CmdTypeDef *cmd) {
-    const uint8_t poly = 0b1011001;
-    const uint8_t seed = 0b111000;
-    //uint8_t index;
-
-    volatile uint64_t data = ((uint64_t)seed << 40) | (*((uint64_t*)cmd) & 0xFFFFFFFFC0);
-    
-    for(int8_t i=39; i>=0; --i) {
-        if(data & ((uint64_t)1 << (i+6))) {
-            data ^= (uint64_t)poly << (i);
-        }
-    }
-
-    return data;
-/*
-    for(int8_t shift=30; shift>=0; shift-=6) {
-        index = ((d >> shift) & 0x3F) ^ crc;
-        crc = crc_table[index];
-    }
-
-    return crc;
-*/
-
-/*
-    // first iteration 
-    _6bit |= (data[0] & 0b11000000) >> 6;
-    _6bit |= (data[1] & 0b00001111) << 2;
-
-    index = (_6bit ^ crc);
-    crc = crc_table[index];
-    // end first iteration
-
-    // second iteration 
-    _6bit = 0;
-    _6bit |= (data[1] & 0b11110000) >> 4;
-    _6bit |= (data[2] & 0b00000011) << 4;
-
-    index = (_6bit ^ crc);
-    crc = crc_table[index];
-    // end second iteration
-
-    // third iteration 
-    _6bit = 0;
-    _6bit |= (data[2] & 0b11111100) >> 2;
-
-    index = (_6bit ^ crc);
-    crc = crc_table[index];
-    // end third iteration
-
-    // fourth iteration 
-    _6bit = 0;
-    _6bit |= (data[3] & 0b00111111);
-
-    index = (_6bit ^ crc);
-    crc = crc_table[index];
-    // end fourth iteration 
-
-    // fifth iteration 
-    _6bit = 0;
-    _6bit |= (data[3] & 0b11000000) >> 6;
-    _6bit |= (data[4] & 0b00001111) << 2;
-
-    index = (_6bit ^ crc);
-    crc = crc_table[index];
-    // end fifth iteration 
-
-    // sixth iteration 
-    _6bit = 0;
-    _6bit |= (data[4] & 0b11110000) >> 4;
-
-    index = (_6bit ^ crc);
-    crc = crc_table[index];
-    // end sixth iteration 
-
-    return crc;
-    */
 }
--- L9963E.h
+++ L9963E.h
@@ -13,55 +13,37 @@
 
 #include "main.h"
 #include "registers.h"
+
 #include <stdint.h>
 
-#define L9963E_CS_HIGH(HANDLE)       HAL_GPIO_WritePin((HANDLE)->cs_port, (HANDLE)->cs_pin, GPIO_PIN_SET)
-#define L9963E_CS_LOW(HANDLE)        HAL_GPIO_WritePin((HANDLE)->cs_port, (HANDLE)->cs_pin, GPIO_PIN_RESET)
+#define L9963E_CS_HIGH(HANDLE) HAL_GPIO_WritePin((HANDLE)->cs_port, (HANDLE)->cs_pin, GPIO_PIN_SET)
+#define L9963E_CS_LOW(HANDLE)  HAL_GPIO_WritePin((HANDLE)->cs_port, (HANDLE)->cs_pin, GPIO_PIN_RESET)
 
-#define L9963E_TXEN_HIGH(HANDLE)       HAL_GPIO_WritePin((HANDLE)->txen_port, (HANDLE)->txen_pin, GPIO_PIN_SET)
-#define L9963E_TXEN_LOW(HANDLE)        HAL_GPIO_WritePin((HANDLE)->txen_port, (HANDLE)->txen_pin, GPIO_PIN_RESET)
+#define L9963E_TXEN_HIGH(HANDLE) HAL_GPIO_WritePin((HANDLE)->txen_port, (HANDLE)->txen_pin, GPIO_PIN_SET)
+#define L9963E_TXEN_LOW(HANDLE)  HAL_GPIO_WritePin((HANDLE)->txen_port, (HANDLE)->txen_pin, GPIO_PIN_RESET)
 
-#define L9963E_BNE_READ(HANDLE)         HAL_GPIO_ReadPin((HANDLE)->bne_port, (HANDLE)->bne_pin)
+#define L9963E_BNE_READ(HANDLE) HAL_GPIO_ReadPin((HANDLE)->bne_port, (HANDLE)->bne_pin)
 
-#define L9963E_CMD_MASK         0xFFFFFFFFFF
+#define L9963E_CMD_MASK 0xFFFFFFFFFF
 
-struct L9963E_DataParamStruct {
-        uint64_t mask;
-        uint8_t offset;
+union L9963E_CmdUnion {
+    uint64_t val;
+    struct {
+        uint64_t crc : 6, data : 18, gsw : 2, addr : 7, devid : 5, rw_burst : 1, pa : 1, : 24;
+    } cmd;
 };
-typedef struct L9963E_DataParamStruct L9963E_DataParamTypeDef;
-
-extern L9963E_DataParamTypeDef L9963E_PA;
-extern L9963E_DataParamTypeDef L9963E_RW;
-extern L9963E_DataParamTypeDef L9963E_BURST;
-extern L9963E_DataParamTypeDef L9963E_DEVID;
-extern L9963E_DataParamTypeDef L9963E_ADDR;
-extern L9963E_DataParamTypeDef L9963E_GSW;
-extern L9963E_DataParamTypeDef L9963E_DATA;
-extern L9963E_DataParamTypeDef L9963E_CRC;
-
-struct L9963E_CmdStruct {
-        uint64_t        crc :6,
-                        data: 18,
-                        gsw :2,
-                        addr :7,
-                        devid :5,
-                        rw_burst :1,
-                        pa :1,
-                        :24;
-};
-typedef struct L9963E_CmdStruct L9963E_CmdTypeDef;
+typedef union L9963E_CmdUnion L9963E_CmdTypeDef;
 
 struct L9963E_HandleStruct {
-        SPI_HandleTypeDef *hspi;
-        GPIO_TypeDef *cs_port;
-        uint16_t cs_pin;
+    SPI_HandleTypeDef *hspi;
+    GPIO_TypeDef *cs_port;
+    uint16_t cs_pin;
 
-        GPIO_TypeDef *txen_port;
-        uint16_t txen_pin;
+    GPIO_TypeDef *txen_port;
+    uint16_t txen_pin;
 
-        GPIO_TypeDef *bne_port;
-        uint16_t bne_pin;
+    GPIO_TypeDef *bne_port;
+    uint16_t bne_pin;
 };
 typedef struct L9963E_HandleStruct L9963E_HandleTypeDef;
 /**
@@ -77,7 +59,15 @@ typedef struct L9963E_HandleStruct L9963E_HandleTypeDef;
  * @param     bne_pin BNE pin
  * @return    HAL_OK on success, HAL_ERROR on failure
  */
-HAL_StatusTypeDef L9963E_init(L9963E_HandleTypeDef *handle, SPI_HandleTypeDef *hspi, GPIO_TypeDef *cs_port, uint16_t cs_pin, GPIO_TypeDef *txen_port, uint16_t txen_pin, GPIO_TypeDef *bne_port, uint16_t bne_pin);
+HAL_StatusTypeDef L9963E_init(
+    L9963E_HandleTypeDef *handle,
+    SPI_HandleTypeDef *hspi,
+    GPIO_TypeDef *cs_port,
+    uint16_t cs_pin,
+    GPIO_TypeDef *txen_port,
+    uint16_t txen_pin,
+    GPIO_TypeDef *bne_port,
+    uint16_t bne_pin);
 /**
  * @brief     Wakes up the IC 
  * 
@@ -94,7 +84,11 @@ HAL_StatusTypeDef L9963E_wakeup(L9963E_HandleTypeDef *handle);
  * @param     data Reference to the structure to be populated with the read data
  * @return    HAL_OK on success, HAL_ERROR on failure
  */
-HAL_StatusTypeDef L9963E_reg_read(L9963E_HandleTypeDef *handle, uint8_t device, L9963E_RegistersAddrTypeDef address, L9963E_RegisterUnionTypeDef *data);
+HAL_StatusTypeDef L9963E_reg_read(
+    L9963E_HandleTypeDef *handle,
+    uint8_t device,
+    L9963E_RegistersAddrTypeDef address,
+    L9963E_RegisterUnionTypeDef *data);
 /**
  * @brief     Writes a register
  * 
@@ -104,13 +98,17 @@ HAL_StatusTypeDef L9963E_reg_read(L9963E_HandleTypeDef *handle, uint8_t device, 
  * @param     data Data to be written
  * @return    HAL_OK on success, HAL_ERROR on failure
  */
-HAL_StatusTypeDef L9963E_reg_write(L9963E_HandleTypeDef *handle, uint8_t device, L9963E_RegistersAddrTypeDef address, L9963E_RegisterUnionTypeDef *data);
+HAL_StatusTypeDef L9963E_reg_write(
+    L9963E_HandleTypeDef *handle,
+    uint8_t device,
+    L9963E_RegistersAddrTypeDef address,
+    L9963E_RegisterUnionTypeDef *data);
 /**
  * @brief     Calculates the CRC6
  * 
- * @param     cmd Data to be used for the calculation
+ * @param     InputWord Data to be used for the calculation
  * @return    the calculated CRC6
  */
-uint8_t L9963E_crc_calc(L9963E_CmdTypeDef *cmd);
+uint8_t L9963E_crc_calc(uint64_t InputWord);
 
-#endif //L9963E_H
+#endif  //L9963E_H
